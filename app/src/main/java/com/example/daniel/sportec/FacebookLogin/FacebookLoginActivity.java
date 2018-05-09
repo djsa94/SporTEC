920f198de686ab48271769f5b25604b107564137
Daniel Sanabria <djsa94@gmail.com>
2018-05-08 21:11:48 -0600

    Login de FB y Firebase creado

---
+++ app/src/main/java/com/example/daniel/sportec/FacebookLogin/FacebookLoginActivity.java
@@ -0,0 +1,161 @@
+package com.example.daniel.sportec.FacebookLogin;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.LoginManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.daniel.sportec.R;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+
+import java.util.Arrays;
+
+
+
+public class FacebookLoginActivity extends AppCompatActivity{
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.facebook_login_layout);
+
+        setTheme(R.style.AppTheme);
+
+        //LoginManager.getInstance().logOut();
+        //Inicializa Firebase
+        mAuth = FirebaseAuth.getInstance();
+        //Callback Manager
+        callbackManager = CallbackManager.Factory.create();
+        //Boton de login creado y especifica permisos
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(
+                "public_profile", "email", "user_birthday", "user_friends"));
+        //Hace callback
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+//                GraphRequest request = GraphRequest.newMeRequest(
+//                        loginResult.getAccessToken(),
+//                        new GraphRequest.GraphJSONObjectCallback() {
+//                            @Override
+//                            public void onCompleted(
+//                                    JSONObject object,
+//                                    GraphResponse response) {
+//                                Log.v("LoginActivity Response ", response.toString());
+//
+//                                try {
+//                                    String Name = object.getString("name");
+//
+//                                    //FEmail = object.getString("email");
+//                                    //Log.v("Email = ", " " + FEmail);
+//                                    Toast.makeText(getApplicationContext(), "Name " + Name, Toast.LENGTH_LONG).show();
+//
+//
+//                                } catch (JSONException e) {
+//                                    e.printStackTrace();
+//                                }
+//                            }
+//                        });
+//                Bundle parameters = new Bundle();
+//                parameters.putString("fields", "id,name,email,gender, birthday");
+//                request.setParameters(parameters);
+//                request.executeAsync();
+
+
+
+            }
+
+
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        //Autentica las credenciales en Firebase
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(FacebookLoginActivity.this, user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(FacebookLoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Revisa si ya hay un user loggeado
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Toast.makeText(FacebookLoginActivity.this, "Firebase logged",
+                    Toast.LENGTH_SHORT).show();
+        }
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken == null;
+
+        if(isLoggedIn){
+            Toast.makeText(FacebookLoginActivity.this, "FB logged.",
+                    Toast.LENGTH_SHORT).show();
+        }
+        //updateUI(currentUser);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
--- app/src/main/java/com/example/daniel/sportec/MainActivity.java
+++ app/src/main/java/com/example/daniel/sportec/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.daniel.sportec;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.daniel.sportec.FacebookLogin.FacebookLoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTheme(R.style.AppTheme);
+
+        Intent intent = new Intent(this, FacebookLoginActivity.class);
+        startActivity(intent);
     }
 }
